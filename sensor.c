8ad099bc2c9efe07ac0d51745a9bead7ae6ad1b9
Bradley Emi <bemi@stanford.edu>
2016-03-05 00:10:41 -0800

    added a second sensor

--- sensor.c
+++ sensor.c
@@ -7,8 +7,10 @@
 #define RPI_INT_DISABLE_2            0x2000B220
 
 const unsigned SENS1 = GPIO_PIN23;
+const unsigned SENS2 = GPIO_PIN25;
 
 const unsigned OUT1 = GPIO_PIN24;
+const unsigned OUT2 = GPIO_PIN26;
 
 void sensor_init(void) {
 	gpio_init();
@@ -19,8 +21,13 @@ void sensor_init(void) {
     gpio_set_pullup(SENS1);
     gpio_detect_rising_edge(SENS1);
 
+    gpio_set_input(SENS2);
+    gpio_set_pullup(SENS2);
+    gpio_detect_rising_edge(SENS2);
+
     // Setup LED outputs
     gpio_set_output(OUT1);
+    gpio_set_output(OUT2);
 
     // Make sure everything is cleared
     PUT32(RPI_INT_DISABLE_1, 0xFFFFFFFF);
@@ -37,8 +44,20 @@ void sensor_init(void) {
 void sensor_int_handler(void) {
 	gpio_check_and_clear_event(SENS1);
 	system_disable_interrupts();
-    gpio_write(OUT1, 1);
+
+	//check which interrupt was triggered
+	if (gpio_read(SENS1) == 1) {
+		gpio_write(OUT1, 1);
+	}
+	if (gpio_read(SENS2) == 1) {
+		gpio_write(OUT2, 1);
+	}
+
     delay(2);
+
+    //turn off the LEDs
     gpio_write(OUT1, 0);
+    gpio_write(OUT2, 0);
+
     system_enable_interrupts();
 }
